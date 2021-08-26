21d3114df2cc05f11b9692e46b79b1f80ca8064e
Sam-Dunne <cras.sdunne@gmail.com>
2021-08-26 03:00:09 -0500

    adding country api call and display functionality. fine tuning responsiveness

--- src/components/Hero/Hero.elements.js
+++ src/components/Hero/Hero.elements.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components/macro';
-import { Section } from '../../globalStyle';
+import { Section, HeroH1, HeroH3 } from '../../globalStyle';
 import { device } from '../../device';
 
 export const HeroContainer = styled(Section)`
@@ -16,7 +16,10 @@ export const HeroContainer = styled(Section)`
       padding: 30px 0;
       margin: 0 auto;
     }
- 
+
+    @media ${device.mobileM} {
+       padding: 15px;
+    }
 `;
 
 export const Input = styled.input`
@@ -26,13 +29,20 @@ export const Input = styled.input`
     padding: 1.4rem;
     width: 311px;
     margin: auto;
+
+    &:focus{
+      outline: none;
+    }
     
 
     @media ${device.tablet} {
       display: inline-block;
       margin: 0;
       width: 220px;
+    }
 
+    @media ${device.mobileM} {
+      max-width: 80vw;
     }
 `;
 
@@ -60,12 +70,16 @@ export const ActionBtn = styled.button`
       margin: 0 12px;
       width: 170px;
     }
+
+    @media ${device.mobileM} {
+      max-width: 80vw;
+    }
 `;
 
 export const HeroImage = styled.img`
     display: block;
     object-fit: cover;
-    margin: 30px auto;
+    margin: 40px auto;
     width: 311px;
     height: 350px;
     border-radius: 15px;
@@ -82,7 +96,67 @@ export const HeroImage = styled.img`
       width: 1080px;
       height: fit-content;
     }
+
+    @media ${device.mobileM} {
+      max-width: 100%;
+    }
 `;
+
+export const GridDataWrapper = styled.div`
+      display: grid;
+      height: fit-content;
+      width: 60%;
+      justify-content: center;
+      align-items: center;
+      gap: 25px;
+      margin: 0 auto;
+      grid-template-columns: 1fr;
+      grid-template-rows:  1fr 1fr 1fr 1fr;
+
+      @media ${device.tablet} {
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows:  1fr 1fr;
+      }
+
+      @media ${device.mobileM} {
+      width: 80%;
+    }
+`;
+
+export const DataItemWrapper = styled.div`
+      background: lightgray;
+      padding: 4rem 5rem;
+      border-radius: 5px;
+      justify-content: center;
+      align-items: center;
+      height: 180px;
+
+      @media ${device.mobileM} {
+       padding: 2rem 3rem;
+       height: 120px;
+    }
+`;
+
+export const DataH1 = styled.h1`
+      font-size: 3rem;
+      width: fit-content;
+      margin: 60px auto 20px auto;
+      text-align: center; 
+      
+`;
+
+export const DataTitle = styled.h4`
+      font-weight: 800;
+      text-align: center;
+      margin: 0 auto 12px auto;
+`;
+
+export const DataH3 = styled.div`
+   
+      text-align: center;
+      
+`;
+
 
 // export const HeroHeaderWrapper = styled.div`
 //     display: flex;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,75 @@
-import React from 'react'
-import { HeroContainer, Input, ActionBtn, HeroImage } from './Hero.elements';
+import React, { useState, useRef, useEffect } from 'react'
+import { HeroContainer, Input, ActionBtn, HeroImage, GridDataWrapper, DataItemWrapper, DataH1, DataTitle, DataH3 } from './Hero.elements';
 import Fade from 'react-reveal/Fade';
 import { HeroH1, HeroH3 } from '../../globalStyle';
 
 const Hero = () => {
+    const inputRef = useRef();
+
+    const [countryName, setCountryName] = useState('');
+    const handleInputOnChange = (e) => setCountryName(e.target.value);
+
+    const [countryData, setCountryData] = useState(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [])
+
+    const handleSubmitCountryName = (e) => {
+        e.preventDefault();
+        if (!countryName) {
+            alert(`User Input is Required`);
+            inputRef.current.focus();
+            return
+        }
+        fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fullText=true`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data.status)
+                if (data.status === 404) {
+                    alert(`Please check your spelling. Error ${data.status} ${data.message}`);
+                    setCountryName('');
+                    inputRef.current.focus();
+                    return;
+                }
+                setCountryData(data[0]);
+                setCountryName('');
+            })
+    }
+
     return (
         <>
             <HeroContainer>
-                <HeroH1>Get the things you need from stores you trust.</HeroH1>
+                <HeroH1 gridArea>Get the things you need from stores you trust.</HeroH1>
                 <HeroH3>Order everything from groceries to household essentials for delivery to your door.</HeroH3>
-                <Input placeholder='Enter Zip Code' ></Input>
-                <ActionBtn>Get started</ActionBtn>
+                <br></br>
+                <HeroH3>This Input fetches data for Countries, and responsively displayed styled with CSS Grid, and has input and response validation, and utilizes hooks useState, useEffect, useRef</HeroH3>
+                <Input type='text' placeholder='Search for Country Data' ref={inputRef} value={countryName} onChange={handleInputOnChange}></Input>
+                <ActionBtn onClick={handleSubmitCountryName}>Get Info</ActionBtn>
+                {countryData &&
+                    <>
+                        <DataH1>{countryData?.name}</DataH1>
+                        <GridDataWrapper>
+                            <DataItemWrapper>
+                                <DataTitle>Capitol</DataTitle>
+                                <DataH3>{countryData?.capital}</DataH3>
+                            </DataItemWrapper>
+                            <DataItemWrapper>
+                                <DataTitle>Population</DataTitle>
+                                <DataH3>{countryData?.population.toLocaleString("en-US")} </DataH3>
+                            </DataItemWrapper>
+                            <DataItemWrapper>
+                                <DataTitle>Currency</DataTitle>
+                                <DataH3>{countryData?.currencies[0]?.name}</DataH3>
+                            </DataItemWrapper>
+                            <DataItemWrapper>
+                                <DataTitle>Subregion</DataTitle>
+                                <DataH3>{countryData?.subregion}</DataH3>
+                            </DataItemWrapper>
+
+                        </GridDataWrapper>
+                    </>
+                }
                 <HeroImage src='https://www.shipt.com/_next/image?url=https%3A%2F%2Fdruven30vo903.cloudfront.net%2Fshipt%2Fweb%2Fassets%2Fmarketing-hero.jpg&w=1080&q=75'></HeroImage>
             </HeroContainer>
             <Fade>

29b8d36acc9a069efc93e28fbf15a9888a17a882
Sam-Dunne <cras.sdunne@gmail.com>
2021-10-14 21:55:47 -0500

    removing dead external api call

--- src/components/Anchor/Anchor.elements.js
+++ src/components/Anchor/Anchor.elements.js
@@ -28,3 +28,4 @@ export const MarketingAnchor = styled.a`
         text-decoration: underline;
     }
 `;
+
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,82 +1,82 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { HeroContainer, Input, ActionBtn, HeroImage, GridDataWrapper, DataItemWrapper, DataH1, DataTitle, DataH3 } from './Hero.elements';
+// import React, { useState, useRef, useEffect } from 'react'
+import { HeroContainer, Input, ActionBtn, HeroImage } from './Hero.elements';
 import Fade from 'react-reveal/Fade';
 import { HeroH1, HeroH3 } from '../../globalStyle';
-import { AlertModal } from '../index';
+// import { AlertModal } from '../index';
 
 const Hero = () => {
-    const inputRef = useRef();
+    // const inputRef = useRef();
 
-    // state to toggle display of AlertModal
-    const [alertDisplayed, setAlertDisplayed] = useState(false);
-    const handleToggleAlertDisplayed = () => {
-        setAlertDisplayed(!alertDisplayed);
-    }
-    // takes one argument for value of string message
-    const [alertMsg, setAlertMsg] = useState('');
-    const handleSetAlertMsg = (msg) => {
-        setAlertMsg(msg)
-    }
-    // stateful value of input and onChangeHandler 
-    const [countryName, setCountryName] = useState('');
-    const handleInputOnChange = (e) => setCountryName(e.target.value);
+    // // state to toggle display of AlertModal
+    // const [alertDisplayed, setAlertDisplayed] = useState(false);
+    // const handleToggleAlertDisplayed = () => {
+    //     setAlertDisplayed(!alertDisplayed);
+    // }
+    // // takes one argument for value of string message
+    // const [alertMsg, setAlertMsg] = useState('');
+    // const handleSetAlertMsg = (msg) => {
+    //     setAlertMsg(msg)
+    // }
+    // // stateful value of input and onChangeHandler 
+    // const [countryName, setCountryName] = useState('');
+    // const handleInputOnChange = (e) => setCountryName(e.target.value);
 
-    // fetched data object from `https://restcountries.eu/rest/v2/name/${countryName}?fullText=true'
-    const [countryData, setCountryData] = useState(null);
+    // // fetched data object from `https://restcountries.eu/rest/v2/name/${countryName}?fullText=true'
+    // const [countryData, setCountryData] = useState(null);
 
   
 
-    // effect runs on intitial page load and when states countryData AND alertDisplayed are false, effect runs focuses countryName input
-    useEffect(() => {
-        if(alertDisplayed) return;
-        // console.log('go')
-        inputRef.current.focus();
-    }, [countryData, alertDisplayed])
+    // // effect runs on intitial page load and when states countryData AND alertDisplayed are false, effect runs focuses countryName input
+    // useEffect(() => {
+    //     if(alertDisplayed) return;
+    //     // console.log('go')
+    //     inputRef.current.focus();
+    // }, [countryData, alertDisplayed])
 
-    // trigger API call
-    const handleSubmitCountryName = (e) => {
-        e.preventDefault();
-        // empty input field validation
-        if (!countryName) {
-            handleSetAlertMsg(`User Input is Required`)
-            // alert(`User Input is Required`);
-            handleToggleAlertDisplayed();
+    // // trigger API call
+    // const handleSubmitCountryName = (e) => {
+    //     e.preventDefault();
+    //     // empty input field validation
+    //     if (!countryName) {
+    //         handleSetAlertMsg(`User Input is Required`)
+    //         // alert(`User Input is Required`);
+    //         handleToggleAlertDisplayed();
            
-            return
-        }
-        fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fullText=true`)
-            .then((res) => res.json())
-            .then((data) => {
-                // uses response object to prevent user mispelling breakage, resets countryName state to empty string, refocuses cursor to input
-                if (data.status === 404) {
-                    handleSetAlertMsg(`Error ${data.status} ${data.message}`);
-                    handleToggleAlertDisplayed();
-                    setCountryName('');
-                    // setCountryData(null)
-                    inputRef.current.focus();
-                    return;
-                }
+    //         return
+    //     }
+    //     fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fullText=true`)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             // uses response object to prevent user mispelling breakage, resets countryName state to empty string, refocuses cursor to input
+    //             if (data.status === 404) {
+    //                 handleSetAlertMsg(`Error ${data.status} ${data.message}`);
+    //                 handleToggleAlertDisplayed();
+    //                 setCountryName('');
+    //                 // setCountryData(null)
+    //                 inputRef.current.focus();
+    //                 return;
+    //             }
                 
-                // setter function for succesful response
-                setCountryData(data[0]);
-                // clears input value state
-                setCountryName('');
-            })
-    }
+    //             // setter function for succesful response
+    //             setCountryData(data[0]);
+    //             // clears input value state
+    //             setCountryName('');
+    //         })
+    // }
 
     return (
         <>
             <HeroContainer>
-                <AlertModal DisplayMe={alertDisplayed} DisplaySetter={setAlertDisplayed} DisplayMsg={alertMsg}></AlertModal>
+                {/* <AlertModal DisplayMe={alertDisplayed} DisplaySetter={setAlertDisplayed} DisplayMsg={alertMsg}></AlertModal> */}
                 <HeroH1 gridArea>Get the things you need from stores you trust.</HeroH1>
                 <HeroH3>Order everything from groceries to household essentials for delivery to your door.</HeroH3>
-                <br></br>
+                {/* <br></br>
                 <HeroH3>This Input fetches Web API data for Countries of the World by name, and displays responsively styled with CSS Grid, and has input and response validation via stateful modal, and utilizes hooks useState, useEffect, useRef.</HeroH3>
                 <Input type='text' placeholder='Enter a World Country name' ref={inputRef} value={countryName} onChange={handleInputOnChange}></Input>
                 {alertDisplayed ? <ActionBtn onClick={handleToggleAlertDisplayed} Hide>Hide Message</ActionBtn> :
-                <ActionBtn onClick={handleSubmitCountryName}>Get Info</ActionBtn>}
+                <ActionBtn onClick={handleSubmitCountryName}>Get Info</ActionBtn>} */}
                         {/* if countryData state not null, then render */}
-                {countryData &&
+                {/* {countryData &&
                     <>
                             <DataH1 >{countryData?.name}</DataH1>
                             <GridDataWrapper>
@@ -98,7 +98,7 @@ const Hero = () => {
                                 </DataItemWrapper>
                             </GridDataWrapper>
                     </>
-                }
+                } */}
                 <HeroImage src='https://www.shipt.com/_next/image?url=https%3A%2F%2Fdruven30vo903.cloudfront.net%2Fshipt%2Fweb%2Fassets%2Fmarketing-hero.jpg&w=1080&q=75'></HeroImage>
             </HeroContainer>
             <Fade>
